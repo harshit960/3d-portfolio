f02810ab4ff4a73c688038a2c51d35e201660478
harshit960 <raj.harshit960@gmail.com>
2025-03-23 20:05:46 +0530

    Add SEO View pages for multiple blog entries

---
+++ app/blog/blackpearl/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const blackpearl = () => {
+    return (
+        <div>
+            <h1>SEO View</h1>
+            <p>Welcome to the SEO view page of the blog.</p>
+        </div>
+    );
+};
+
+export default blackpearl;
