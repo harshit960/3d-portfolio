7d50cb2952be23028d9c7b4cf6a6fe12c437e5a4
harshit960 <raj.harshit960@gmail.com>
2025-05-04 10:53:51 +0530

    Add Google Analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import { montserrat, poppins } from '@/constant/font'
 import './globals.css'
 import Navbar from '../components/Navbar'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = {
   title: "Home - Harshit's Portfolio",
@@ -20,6 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${montserrat.variable} ${poppins.variable} lg:overflow-x-hidden`}>
       <body	className='bg-black' >
+      <GoogleAnalytics gaId="G-WEJXY46R8L" />
+
         <Navbar />
         {children}
       </body>
